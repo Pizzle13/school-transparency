dab12ce9db079bbf1d2c2aa93fd57037b4215ead
Pizzle13 <jpbhikarry@gmail.com>
2026-01-15 10:28:08 +0500

    Update webpack hot module hashes and globals.css export
    Changed the hash values in webpack runtime and updated the default export in globals.css. Added new hot-update files for app/layout and webpack to support hot module replacement.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bdd626d210326f85"; }
+/******/ 		__webpack_require__.h = function() { return "8269b93fbf75deed"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
