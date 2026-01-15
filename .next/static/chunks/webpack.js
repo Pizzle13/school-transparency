f4cc923f2f1154b5310cf9e2646d3a7c411abfb9
Pizzle13 <jpbhikarry@gmail.com>
2026-01-15 10:38:54 +0500

    Update build artifacts and hot-update files
    Regenerated .next build and cache files, including updated layout.js and webpack.js chunks. Added new webpack hot-update files reflecting recent changes to the app layout and hash values.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "74fc87411d0d1657"; }
+/******/ 		__webpack_require__.h = function() { return "a3afb7e754c790ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
